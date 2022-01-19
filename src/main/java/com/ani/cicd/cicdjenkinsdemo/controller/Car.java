c9a815f940597cb26d2b96d3e625500d3a3bb815
divya.ar <divya.ar@collabera.com>
2022-01-19 14:29:26 +0530

    dcoker image

---
+++ src/main/java/com/ani/cicd/cicdjenkinsdemo/controller/Car.java
@@ -0,0 +1,24 @@
+package com.ani.cicd.cicdjenkinsdemo.controller;
+
+public class Car {
+
+    //state
+    public int speed;
+
+
+    // constructor used to initialize the state at the time of
+    // object creation
+    //default
+    public Car() {
+        speed = 10;
+    }
+
+    //parameterized
+    public Car(int sp){
+        speed = sp;
+    }
+
+    public void speedUp(){
+        speed += 10;
+    }
+}
